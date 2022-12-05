20a4647f36beedba5c763e700bfaf57c5c7d39fd
Dulebova Emma <emma2105@yandex.ru>
2022-12-05 19:14:37 +0300

    add ConsoleApp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите значение массива: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "   ");        
+    }
+}
+
+void ChangeArray (string[] array)
+{
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[i] = array[i];
+            Console.Write(result[i] + "   ");
+        }
+    }
+}
+
+Console.WriteLine("Введите длину массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[m];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+Console.WriteLine("Массив из строк, длина которых меньше либо равна 3 символа: ");
+ChangeArray (array);
+
+
+
+
+
